77ca381bb0190e6f379b47995c77e59a541c95da
Александр <julcapp@gmail.com>
2026-08-20 15:38:51 +0700

    Register Referral Core module

--- backend/src/modules/index.js
+++ backend/src/modules/index.js
@@ -20,6 +20,7 @@ const videoSurveillance = require('./video_surveillance');
 const eventCenter = require('./event_center');
 const organization = require('./organization');
 const botCore = require('./bot_core');
+const referral = require('./referral');
 
 const moduleManifests = [
   customer,
@@ -44,6 +45,7 @@ const moduleManifests = [
   eventCenter,
   organization,
   botCore,
+  referral,
 ];
 
 module.exports = {
