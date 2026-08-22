309a939640734c963ecda0c9c50fbd13b704722b
Александр <julcapp@gmail.com>
2026-08-22 20:12:38 +0700

    feat: attach camera photo intake runtime

--- backend/src/photoVerificationRuntime.js
+++ backend/src/photoVerificationRuntime.js
@@ -1,20 +1,32 @@
 const { getPrismaClient } = require('./common/database');
 const {
   PrismaPhotoVerificationRepository,
+  PrismaPhotoSubmissionRepository,
+  LocalPhotoStorageAdapter,
+  PhotoSubmissionIntakeService,
+  PhotoCustomerWorkflow,
   PhotoVerificationAdminService,
   PhotoPublicationReadModel,
 } = require('./modules/photo_verification');
 
 function attachPhotoVerificationRuntime(dependencies, { prisma } = {}) {
   if (!dependencies) throw new Error('dependencies are required');
-  if (dependencies.photoVerificationAdminService && dependencies.photoPublicationReadModel) return dependencies;
 
   const db = prisma || getPrismaClient();
   const repository = dependencies.photoVerificationRepository || new PrismaPhotoVerificationRepository(db);
+  const submissionRepository = dependencies.photoSubmissionRepository || new PrismaPhotoSubmissionRepository(db);
+  const customerWorkflow = dependencies.photoCustomerWorkflow || new PhotoCustomerWorkflow({ repository });
 
   dependencies.photoVerificationRepository = repository;
+  dependencies.photoSubmissionRepository = submissionRepository;
+  dependencies.photoCustomerWorkflow = customerWorkflow;
   dependencies.photoVerificationAdminService = dependencies.photoVerificationAdminService || new PhotoVerificationAdminService({ repository });
   dependencies.photoPublicationReadModel = dependencies.photoPublicationReadModel || new PhotoPublicationReadModel({ repository });
+  dependencies.photoSubmissionIntakeService = dependencies.photoSubmissionIntakeService || new PhotoSubmissionIntakeService({
+    repository: submissionRepository,
+    storage: dependencies.photoStorage || new LocalPhotoStorageAdapter(),
+    customerWorkflow,
+  });
 
   return dependencies;
 }
