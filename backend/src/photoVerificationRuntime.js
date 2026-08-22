96c26d3ccb7008219f1a64805baf62de62445af2
Александр <julcapp@gmail.com>
2026-08-22 20:28:02 +0700

    feat: attach CRM notifier to photo workflow

--- backend/src/photoVerificationRuntime.js
+++ backend/src/photoVerificationRuntime.js
@@ -5,20 +5,23 @@ const {
   LocalPhotoStorageAdapter,
   PhotoSubmissionIntakeService,
   PhotoCustomerWorkflow,
+  CrmPhotoNotifier,
   PhotoVerificationAdminService,
   PhotoPublicationReadModel,
 } = require('./modules/photo_verification');
 
-function attachPhotoVerificationRuntime(dependencies, { prisma } = {}) {
+function attachPhotoVerificationRuntime(dependencies, { prisma, logger } = {}) {
   if (!dependencies) throw new Error('dependencies are required');
 
   const db = prisma || getPrismaClient();
   const repository = dependencies.photoVerificationRepository || new PrismaPhotoVerificationRepository(db);
   const submissionRepository = dependencies.photoSubmissionRepository || new PrismaPhotoSubmissionRepository(db);
-  const customerWorkflow = dependencies.photoCustomerWorkflow || new PhotoCustomerWorkflow({ repository });
+  const notifier = dependencies.photoNotifier || new CrmPhotoNotifier({ crmRuntime: dependencies.crmRuntime, logger });
+  const customerWorkflow = dependencies.photoCustomerWorkflow || new PhotoCustomerWorkflow({ repository, notifier });
 
   dependencies.photoVerificationRepository = repository;
   dependencies.photoSubmissionRepository = submissionRepository;
+  dependencies.photoNotifier = notifier;
   dependencies.photoCustomerWorkflow = customerWorkflow;
   dependencies.photoVerificationAdminService = dependencies.photoVerificationAdminService || new PhotoVerificationAdminService({ repository });
   dependencies.photoPublicationReadModel = dependencies.photoPublicationReadModel || new PhotoPublicationReadModel({ repository });
