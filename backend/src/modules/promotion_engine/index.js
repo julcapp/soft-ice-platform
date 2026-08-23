f44570016d6fe7e2e0d8a58e4a02b25e822c696a
Александр <julcapp@gmail.com>
2026-08-23 17:26:40 +0700

    feat(pricing): export pricing engine services

--- backend/src/modules/promotion_engine/index.js
+++ backend/src/modules/promotion_engine/index.js
@@ -2,6 +2,9 @@ const { PromotionValidationService } = require('./PromotionValidationService');
 const { PromotionSafetyService } = require('./PromotionSafetyService');
 const { PromotionRepository } = require('./PromotionRepository');
 const { PromotionService } = require('./PromotionService');
+const { PricingEngineService } = require('./PricingEngineService');
+const { PricingRepository } = require('./PricingRepository');
+const { ActivePromotionResolver } = require('./ActivePromotionResolver');
 
 module.exports = {
   name: 'promotion_engine',
@@ -13,9 +16,14 @@ module.exports = {
     'promotion compatibility policy',
     'promotion approval workflow',
     'promotion runtime safety',
+    'server-side pricing quotes',
+    'promotion price locks and snapshots',
   ],
   PromotionValidationService,
   PromotionSafetyService,
   PromotionRepository,
   PromotionService,
+  PricingEngineService,
+  PricingRepository,
+  ActivePromotionResolver,
 };
