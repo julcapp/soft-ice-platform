9210190211c3e49256580bf8a44cb8ecf98ee77f
Александр <julcapp@gmail.com>
2026-09-17 14:35:15 +0700

    load approved terminal visual fixes

--- frontend/terminal/src/main.jsx
+++ frontend/terminal/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './agreed-flow.css';
+import './visual-fixes.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
